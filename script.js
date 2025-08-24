f90a6b115bb690446b675387c402ddb90aadad7f
tsioryFanamby <mbyfana603@gmail.com>
2025-08-24 23:27:23 +0300

    begining of , dom interaction

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const timer = document.querySelector('.timer');
+var milliSecond = document.getElementById("milliSecond");
+var second = document.getElementById("second");
+
+function spacebar(){
+    document.addEventListener( 'keydown',(event) =>  {
+        if( event.code === 'Space'){
+            event.preventDefault();
+            if(!toggled){
+                timer.style.backgroundColor = 'black';
+                milliSecond.textContent = '30';
+                second.textContent = '60';
+            }
+            else{
+                
+            }
+            
+        }
+    });
+}
+spacebar();
 
